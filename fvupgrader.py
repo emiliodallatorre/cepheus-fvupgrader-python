e4fe1ebda7de6c2cb1f1441c4af90e0bae0e45ea
Emilio Dalla Torre <info@emiliodallatorre.it>
2024-02-15 20:26:32 +0100

    Fixed code style with Pylint

--- fvupgrader.py
+++ fvupgrader.py
@@ -1,3 +1,5 @@
+""" fvupgrader.py: A simple script to upgrade the version of a Flutter project. """
+
 #!/usr/bin/env python3
 
 import os.path
@@ -10,17 +12,49 @@ version_regex: str = r"version: (\d+\.\d+\.\d+\+\d+)"
 version_file: str = "pubspec.yaml"
 
 
+class GitOperationException(Exception):
+    """Exception raised for Git operation errors.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
 def get_version(directory_path: str) -> str:
-    with open(f"{directory_path}/{version_file}", "r") as file:
+    """
+    Retrieves the version number from a file located in the specified directory.
+
+    Args:
+        directory_path (str): The path to the directory containing the version file.
+
+    Returns:
+        str: The version number extracted from the file.
+
+    Raises:
+        Exception: If the version number is not found in the file.
+    """
+    with open(f"{directory_path}/{version_file}", "r", encoding="utf-8") as file:
         content = file.read()
         match = re.search(version_regex, content)
         if match:
             return match.group(1)
         else:
-            raise Exception("Version not found in pubspec.yaml")
+            raise FileNotFoundError("Version not found in pubspec.yaml")
 
 
 def get_available_next_versions(directory_path: str) -> list[str]:
+    """
+    Returns a list of available next versions based on the current version of the directory.
+
+    Args:
+        directory_path (str): The path to the directory.
+
+    Returns:
+        list[str]: A list of available next versions.
+    """
     current_version = get_version(directory_path)
     current_version = current_version.replace("+", ".")
     major, minor, patch, build = current_version.split(".")
@@ -32,48 +66,99 @@ def get_available_next_versions(directory_path: str) -> list[str]:
 
 
 def update_version(new_version: str, directory_path: str) -> None:
-    with open(f"{directory_path}/{version_file}", "r") as file:
+    """
+    Update the version number in a file located at the specified directory path.
+
+    Args:
+        new_version (str): The new version number to update.
+        directory_path (str): The path to the directory containing the version file.
+
+    Returns:
+        None
+    """
+    with open(f"{directory_path}/{version_file}", "r", encoding="utf-8") as file:
         content = file.read()
         new_content = re.sub(version_regex, f"version: {new_version}", content)
-    with open(f"{directory_path}/{version_file}", "w") as file:
+    with open(f"{directory_path}/{version_file}", "w", encoding="utf-8") as file:
         file.write(new_content)
 
 
-def git_operations(new_version: str, directory_path: str, args) -> None:
+def git_operations(new_version: str, directory_path: str, parsed_args) -> None:
+    """
+    Perform git operations such as committing changes, tagging releases, and pushing changes.
+
+    Args:
+        new_version (str): The new version to be used for the commit and tag.
+        directory_path (str): The path to the directory where the git repository is located.
+        parsed_args: The parsed command-line arguments.
+
+    Returns:
+        None
+    """
     # Commit the changes
-    if not args.no_commit:
+    if not parsed_args.no_commit:
         os.system(f"git -C {directory_path} add {version_file}")
         os.system(
             f"git -C {directory_path} commit -am 'Bump version to {new_version}'")
 
     # Tag the release
-    if not args.no_tag:
+    if not parsed_args.no_tag:
         os.system(f"git -C {directory_path} tag {new_version}")
         print(f"Tagged release {new_version}")
 
     # Push the changes
-    if not args.no_push:
+    if not parsed_args.no_push:
         os.system(f"git -C {directory_path} push")
 
 
 def is_dir_git_repo(directory_path: str) -> bool:
+    """
+    Check if a directory is a Git repository.
+
+    Args:
+        directory_path (str): The path to the directory.
+
+    Returns:
+        bool: True if the directory is a Git repository, False otherwise.
+    """
     return os.path.isdir(directory_path + "/.git")
 
 
 def is_dir_flutter_project(directory_path: str) -> bool:
+    """
+    Check if the given directory contains a Flutter project.
+
+    Args:
+        directory_path (str): The path to the directory.
+
+    Returns:
+        bool: True if the directory contains a Flutter project, False otherwise.
+    """
     return os.path.isfile(directory_path + "/pubspec.yaml")
 
 
-def main(args) -> None:
-    directory_path = os.path.abspath(args.path)
+def main(parsed_args) -> None:
+    """
+    Main function for upgrading a Flutter project to a new version.
+
+    Args:
+        parsed_args: Parsed command-line arguments.
+
+    Raises:
+        FileNotFoundError: If the provided path is not a Flutter project.
+
+    Returns:
+        None
+    """
+    directory_path = os.path.abspath(parsed_args.path)
 
     if not is_dir_flutter_project(directory_path):
-        raise Exception("This is not a Flutter project")
+        raise FileNotFoundError("This is not a Flutter project")
 
     print("Current version:", get_version(directory_path))
     available_versions = get_available_next_versions(directory_path)
 
-    if not args.major and not args.minor and not args.patch:
+    if not parsed_args.major and not parsed_args.minor and not parsed_args.patch:
         print("Available next versions:")
         for index, version in enumerate(available_versions):
             print(f"{index + 1}. {version}")
@@ -81,11 +166,11 @@ def main(args) -> None:
         new_version_number = input("Choose a new version: ")
         new_version = available_versions[int(new_version_number) - 1]
     else:
-        if args.major:
+        if parsed_args.major:
             new_version = available_versions[2]
-        elif args.minor:
+        elif parsed_args.minor:
             new_version = available_versions[1]
-        elif args.patch:
+        elif parsed_args.patch:
             new_version = available_versions[0]
 
     print(f"Updating to {new_version}...")
@@ -94,53 +179,73 @@ def main(args) -> None:
 
     new_git_version: str = f"v{new_version}"
     if is_dir_git_repo(directory_path):
-        git_operations(new_git_version, directory_path, args)
+        git_operations(new_git_version, directory_path, parsed_args)
 
 
-def fix_args(args) -> ArgumentParser:
-    if not args.no_push and args.no_commit:
-        raise Exception("You cannot push without committing")
-    if not args.no_tag and args.no_commit:
-        raise Exception("You cannot tag without committing")
+def fix_args(parsed_args) -> ArgumentParser:
+    """
+    Validates the command line arguments and raises exceptions if invalid combinations are detected.
 
-    if args.major and args.minor:
-        raise Exception(
+    Args:
+        args (ArgumentParser): The command line arguments.
+
+    Raises:
+        GitOperationException: If the combination of arguments is invalid.
+
+    Returns:
+        ArgumentParser: The validated command line arguments.
+    """
+    if not parsed_args.no_push and parsed_args.no_commit:
+        raise GitOperationException("You cannot push without committing")
+    if not parsed_args.no_tag and parsed_args.no_commit:
+        raise GitOperationException("You cannot tag without committing")
+
+    if parsed_args.major and parsed_args.minor:
+        raise GitOperationException(
             "You cannot upgrade the major and minor versions at the same time")
-    if args.major and args.patch:
-        raise Exception(
+    if parsed_args.major and parsed_args.patch:
+        raise GitOperationException(
             "You cannot upgrade the major and patch versions at the same time")
-    if args.minor and args.patch:
-        raise Exception(
+    if parsed_args.minor and parsed_args.patch:
+        raise GitOperationException(
             "You cannot upgrade the minor and patch versions at the same time")
 
-    return args
+    return parsed_args
 
 
 if __name__ == "__main__":
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument(
-        "--path", help="path to the directory containing the pubspec.yaml file", type=str, default=".", required=False
+        "--path", help="path to the directory containing the pubspec.yaml file",
+        type=str, default=".", required=False
     )
     parser.add_argument(
-        "--no-push", help="push the changes to the git repository", action="store_true", default=False
+        "--no-push", help="push the changes to the git repository",
+        action="store_true", default=False
     )
     parser.add_argument(
-        "--no-commit", help="commit the changes to the git repository", action="store_true", default=False
+        "--no-commit", help="commit the changes to the git repository",
+        action="store_true", default=False
     )
     parser.add_argument(
-        "--no-tag", help="tag the release in the git repository", action="store_true", default=False
+        "--no-tag", help="tag the release in the git repository",
+        action="store_true", default=False
     )
     parser.add_argument(
-        "--major", help="upgrade the major version", action="store_true", default=False
+        "--major", help="upgrade the major version",
+        action="store_true", default=False
     )
     parser.add_argument(
-        "--minor", help="upgrade the minor version", action="store_true", default=False
+        "--minor", help="upgrade the minor version",
+        action="store_true", default=False
     )
     parser.add_argument(
-        "--patch", help="upgrade the patch version", action="store_true", default=False
+        "--patch", help="upgrade the patch version",
+        action="store_true", default=False
     )
     parser.add_argument(
-        "--version", action="version", version=f"%(prog)s v{fvupgrader_version}"
+        "--version",
+        action="version", version=f"%(prog)s v{fvupgrader_version}"
     )
 
     args = parser.parse_args()
